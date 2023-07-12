eeae37e9079ea7b2245a7b01d1b359146cc0badb
Yogesh <chauhanyogesh950@gmail.com>
2023-07-12 17:24:35 +0530

    Promise Assignment

---
+++ Promise Assignment/New Assignmnert/ODD Even Template_670476/ODD Even Template/src/oddEven.js
@@ -0,0 +1,24 @@
+function getData(data) {
+    let promise1 = new Promise(function(resolve,reject){
+        if (data % 2 == 0){
+            setTimeout(function(){
+                resolve("Even")
+            },4000)
+        }
+        else if (data % 2 == 1){
+            setTimeout(function(){
+                resolve("Odd")
+            },2000)
+        }
+        else{reject("Error")}
+    })
+    return promise1
+}
+// getData(10).then(function(data1){
+//     console.log(data1)}).catch(function(data2){
+//     console.log(data2)})
+
+
+export default getData
+
+
